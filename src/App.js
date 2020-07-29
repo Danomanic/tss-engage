eb67e40aee7f1c2b6753407e54914d045dd9cb75
Daniel Pomfret <dan@coly.uk>
2020-07-29 20:54:34 +0100

    feat(*): login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
+
+  const [error, setError] = React.useState(null);
+  const [isLoaded, setIsLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3002/auth/authenticated")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          console.log(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Login />
   );
 }
 
